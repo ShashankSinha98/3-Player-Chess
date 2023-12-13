c97fb6d6bd2581003ffebc60c785c14b30006367
Stefan Schulze <stefan.schulze@siemens.com>
2023-12-13 17:56:38 +0100

    added JSDoc comments

--- webapp/src/main/resources/static/scripts/game.js
+++ webapp/src/main/resources/static/scripts/game.js
@@ -1,3 +1,26 @@
+/**
+ * maps the piece token to the corresponding unicode character
+ * @type {{P: string, Q: string, R: string, B: string, K: string, N: string}}
+ */
+const pieceMap = {
+    'R': '♖',
+    'N': '♘',
+    'B': '♗',
+    'Q': '♕',
+    'K': '♔',
+    'P': '♙'
+};
+
+/**
+ * maps the letters R,G,B to the corresponding color string
+ * @type {{R: string, B: string, G: string}}
+ */
+const colorMap = {'R':'Red', 'G':'Green', 'B': 'Blue'};
+
+/**
+ * global variable storing the current theme
+ * @type {string}
+ */
 let theme = 'arialTheme'
 
 document.addEventListener('DOMContentLoaded', function () {
@@ -38,12 +61,20 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
+/**
+ * displays the current player inside p element with the id "playerdisplay"
+ * @param color color of the current player as single character (R, G, B)
+ */
 function updateCurrenPlayer(color){
     console.log("Current player: " + color);
     const p = document.getElementById('playerdisplay');
     p.textContent = colorMap[color]
 }
 
+/**
+ * updates the current theme
+ * @param name name of the theme (arialTheme, freeSerifTheme, dejaVuSansTheme)
+ */
 function updateTheme(name){
     console.log('New Font: ' + name);
     theme = name;
@@ -53,8 +84,12 @@ function updateTheme(name){
     });
 }
 
+/**
+ * highlights a specific set of squares
+ * @param data set of squares as JSON array
+ */
 function displayPossibleMoves(data){
-    console.log(data);
+    console.log('Highlighted Squares: ' + data);
     removeHighlighting();
     data.forEach(function (squareId) {
         const polygon = document.getElementById(squareId);
@@ -65,27 +100,18 @@ function displayPossibleMoves(data){
     });
 }
 
+/**
+ * removes the highlighting of all squares
+ */
 function removeHighlighting(){
     const polygons = document.querySelectorAll('polygon');
     polygons.forEach( polygon => polygon.classList.remove('highlight'));
 }
 
-function hasHighlighting(){
-    const polygons = document.querySelectorAll('polygon.highlight');
-    return polygons.length > 0;
-}
-
-const pieceMap = {
-    'R': '♖',
-    'N': '♘',
-    'B': '♗',
-    'Q': '♕',
-    'K': '♔',
-    'P': '♙'
-};
-
-const colorMap = {'R':'Red', 'G':'Green', 'B': 'Blue'};
-
+/**
+ * updates the chessboard with the new state
+ * @param data new board state with the updated piece positions, e.g. {Ba1: "BR", Ba2: "BP", ...}
+ */
 function updateBoard(data) {
     clearBoard();
     console.log('New Board Configuration:', data);
@@ -101,21 +127,33 @@ function updateBoard(data) {
     }
 }
 
+/**
+ * displays the piece as a textElement inside svg
+ * @param squareId Id of the square, e.g. Ba1, Ba2, ...
+ * @param pieceToken token of the piece, e.g. R, N, B, K, Q, P
+ * @param pieceColor color as single character, e.g. R, G, B
+ */
 function displayPiece(squareId, pieceToken, pieceColor) {
     //console.log(squareId);
     const square = document.getElementById(squareId);
     //console.log(square);
-    const punkte = square.points;
+    const points = square.points;
 
-    let x = (punkte.getItem(0).x + punkte.getItem(2).x)/2;
-    let y = (punkte.getItem(0).y + punkte.getItem(2).y)/2;
+    let x = (points.getItem(0).x + points.getItem(2).x)/2;
+    let y = (points.getItem(0).y + points.getItem(2).y)/2;
 
     const textElement = getPieceText(x, y, pieceColor, pieceToken );
-    //textElement.onmouseover = function () {addHoverEffect(square)}
-    //textElement.onmouseout = function () {removeHoverEffect(square)}
     square.parentNode.insertBefore(textElement, square.nextSibling);
 }
 
+/**
+ * creates a new svg text element displaying a piece
+ * @param x coordinate of the text element
+ * @param y coordinate of the text element
+ * @param color color of the displayed piece, e.g. R, G, B
+ * @param pieceToken token of the displayed piece, e.g. R, N, B, K, Q, P
+ * @returns {SVGTextElement}
+ */
 function getPieceText(x, y, color, pieceToken) {
     const textElement = document.createElementNS('http://www.w3.org/2000/svg', 'text');
     textElement.setAttribute('x', x);
@@ -133,7 +171,9 @@ function getPieceText(x, y, color, pieceToken) {
     return textElement;
 }
 
-
+/**
+ * removes all displayed pieces from the chessboard
+ */
 function clearBoard() {
     const textElements = document.querySelectorAll('text');
     textElements.forEach((textElement) => {
