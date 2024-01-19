2974f55c4adfda1dc7930160874c8a1addfcee74
Shashank Sinha <sinhashashank.98@gmail.com>
2024-01-19 19:18:30 +0100

    Add unit tests for BoardTest

--- backend/src/test/java/model/BoardTest.java
+++ backend/src/test/java/model/BoardTest.java
@@ -1,11 +1,21 @@
 package model;
 
+import common.Colour;
+import common.InvalidMoveException;
+import common.InvalidPositionException;
+import common.Position;
 import model.Board;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import utility.Log;
 
 import java.util.HashSet;
 import java.util.Set;
+
+import static common.Position.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class BoardTest {
 
@@ -28,54 +38,88 @@ public class BoardTest {
     }
 
     // Naming Convention- MethodName_StateUnderTest_ExpectedBehavior
-/*    @Test
-    public void isEmpty_outOfBoundIndex_Exception() {
-        assertThrows(InvalidPositionException.class,
-                () -> {board.isEmpty(-1);});
-
-        assertThrows(InvalidPositionException.class,
-                () -> {board.isEmpty(96);});
+    @Test
+    public void move_pieceMoveToEmptyPolygon_startPositionEmptyAndEndPositionOccupied() throws InvalidPositionException, InvalidMoveException {
+        BasePiece pawn = board.boardMap.get(BE2);
+        board.move(BE2, BE4);
+        assertNull(board.boardMap.get(BE2));
+        assertNotNull(board.boardMap.get(BE4));
+        assertEquals(pawn, board.boardMap.get(BE4));
     }
 
     @Test
-    public void isEmpty_nonEmptyIndex_False() throws InvalidPositionException {
-        for(int position: startingPiecesIndexes) {
-            assertFalse(board.isEmpty(position));
-        }
-    }*/
+    public void move_wallMoveToEmptyPolygon_startPositionEmptyAndEndPositionOccupiedAndWallMappingUpdate() throws InvalidPositionException, InvalidMoveException {
+        BasePiece wall = board.boardMap.get(BH2);
+        assertTrue(wall instanceof Wall);
+        assertEquals(Colour.BLUE, wall.getColour());
 
-/*    @Test
-    public void isEmpty_emptyIndex_True() throws InvalidPositionException {
-        for(int position=0; position< TOTAL_SQUARES; position++) {
-            if(!startingPiecesIndexes.contains(position)) {
-                assertTrue(board.isEmpty(position));
-            }
-        }
-    }*/
+        board.move(BH2, BH4);
+        assertNull(board.boardMap.get(BH2));
+        assertNotNull(board.boardMap.get(BH4));
+        assertEquals(wall, board.boardMap.get(BH4));
+        assertEquals(BH4, board.wallPieceMapping.get(wall));
+    }
 
-    // move function currently not checking for legal move. must check it later
+    @Test
+    public void move_pawnToOppositeEndRow_pawnUpgradeToQueen() throws InvalidPositionException, InvalidMoveException {
+        BasePiece bluePawn = new Pawn(Colour.BLUE);
+        board.boardMap.put(RA2, bluePawn);
+        board.boardMap.remove(RA1); // empty RA1 for blue pawn to move
+        board.move(RA2, RA1);
 
-/*    @ParameterizedTest
-    @EnumSource(Position.class)
-    public void move_emptyStartPosition_noUpdateInBoard(Position startPos) {
-        Map<Position, Piece> boardMapCopy = new HashMap<>(board.getBoardMap()); // copy original board map
-        if(!startingPiecesIndexes.contains(startPos.getValue())) { // empty polygon
-            board.move(startPos, Position.BA1);
-            assertEquals(boardMapCopy.get(startPos), board.getBoardMap().get(startPos));
-        }
-    }*/
+        BasePiece promotedPiece = board.boardMap.get(RA1);
+        assertTrue(promotedPiece instanceof Queen);
+    }
 
-/*    @ParameterizedTest
-    @EnumSource(Position.class)
-    public void move_nonEmptyStartPosition_updateInBoard(Position startPos) {
-        Map<Position, Piece> boardMapCopy = new HashMap<>(board.getBoardMap()); // copy original board map
-        Map<Position, Piece> boardMap = board.getBoardMap();
-        if(startingPiecesIndexes.contains(startPos.getValue())) {  // non-empty polygon
-            Position endPos = Position.BA3;
-            Piece movedPiece = boardMapCopy.get(startPos);
-            board.move(startPos, endPos);
-            assertNull(boardMap.get(startPos));
-            assertEquals(movedPiece, boardMap.get(endPos));
-        }
-    }*/
+    @Test
+    public void move_jesterTakesWall_jesterWallPositionSwitchAndWallPieceMappingUpdates() throws InvalidPositionException, InvalidMoveException {
+        BasePiece blueJester = new Jester(Colour.BLUE);
+        board.boardMap.put(BE4, blueJester);
+
+        BasePiece redWall = board.boardMap.get(RH2);
+
+        board.boardMap.put(RC3, redWall);
+        board.move(BE4, RC3);
+        assertEquals(redWall, board.boardMap.get(BE4));
+        assertEquals(blueJester, board.boardMap.get(RC3));
+        assertEquals(BE4, board.wallPieceMapping.get(redWall));
+    }
+
+    @Test
+    public void move_rightCastlingLegalMove_castlingHappen() throws InvalidPositionException, InvalidMoveException {
+        board.boardMap.remove(BF1);
+        board.boardMap.remove(BG1);
+
+        BasePiece king = board.boardMap.get(BE1);
+        BasePiece rightRook = board.boardMap.get(BH1);
+
+        board.move(BE1, BG1); // left castling
+        assertEquals(king, board.boardMap.get(BG1));
+        assertEquals(rightRook, board.boardMap.get(BF1));
+    }
+
+    @Test
+    public void move_leftCastlingLegalMove_castlingHappen() throws InvalidPositionException, InvalidMoveException {
+        board.boardMap.remove(BD1);
+        board.boardMap.remove(BC1);
+        board.boardMap.remove(BB1);
+
+        BasePiece king = board.boardMap.get(BE1);
+        BasePiece leftRook = board.boardMap.get(BA1);
+
+        board.move(BE1, BC1); // left castling
+        assertEquals(king, board.boardMap.get(BC1));
+        assertEquals(leftRook, board.boardMap.get(BD1));
+    }
+
+    @Test
+    public void move_bluePieceTakesRedKing_gameOverAndBlueWinner() throws InvalidPositionException, InvalidMoveException {
+        BasePiece blueRook = new Rook(Colour.BLUE);
+        board.boardMap.put(RE2, blueRook);
+
+        board.move(RE2, RE1);
+
+        assertTrue(board.isGameOver());
+        assertEquals("B", board.getWinner());
+    }
 }
