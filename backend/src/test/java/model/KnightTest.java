e79fdd5720c4bc1367071b53ae273b593525c92e
Stefan Schulze <stefan.schulze@siemens.com>
2024-01-24 23:57:52 +0100

    added parameterized tests in KnightTest.java

--- backend/src/test/java/model/KnightTest.java
+++ backend/src/test/java/model/KnightTest.java
@@ -1,10 +1,13 @@
 package model;
 
+import com.google.common.collect.ImmutableSet;
 import common.Colour;
 import common.Position;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.Arrays;
 import java.util.HashSet;
@@ -15,135 +18,67 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class KnightTest {
     // Naming Convention- MethodName_StateUnderTest_ExpectedBehavior
+    private Board board;
+
+    @BeforeEach
+    void initBeforeEachBoardTest() {
+        board = new Board();
+    }
+
     @Test
     void setupDirections_initPieceDirectionsIsEmpty_False(){
         BasePiece knight = new Knight(Colour.BLUE);
         assertNotEquals(0, knight.directions.length);
     }
 
-    @Test
-    void isLegalMove_knightTakesItsColourPiece_False() {
+    @ParameterizedTest
+    @EnumSource(Colour.class)
+    void isLegalMove_knightMovesToEmptySquare_True(Colour colour) {
         Board board = new Board();
         board.boardMap.clear();
 
-        BasePiece blueKnight = new Knight(Colour.BLUE);
-        board.boardMap.put(BE2, blueKnight);
+        Position knightPosition = BE2;
 
-        BasePiece bluePawn = new Pawn(Colour.BLUE);
-        board.boardMap.put(BF4, bluePawn);
+        BasePiece knight = new Knight(colour);
+        board.boardMap.put(knightPosition, knight);
 
-        assertFalse(blueKnight.isLegalMove(board, BE4, BD3));
+        assertTrue(knight.isLegalMove(board, knightPosition, BF4));
     }
 
-    @Test
-    void isLegalMove_knightTakesDifferentColourPiece_True() {
-        Board board = new Board();
-        board.boardMap.clear();
+    @ParameterizedTest
+    @MethodSource("model.DataProvider#pieceProvider")
+    void isLegalMove_knightTakesItsColourPiece_False(BasePiece piece) {
+        BasePiece knight = new Knight(piece.colour);
 
-        BasePiece blueKnight = new Knight(Colour.BLUE);
-        board.boardMap.put(BE2, blueKnight);
+        board.boardMap.put(BE4, knight);
+        board.boardMap.put(BC3, piece);
 
-        BasePiece redPawn = new Pawn(Colour.RED);
-        board.boardMap.put(BF4, redPawn);
-
-        assertTrue(blueKnight.isLegalMove(board, BE2, BF4));
+        assertFalse(knight.isLegalMove(board, BE4, BC3));
     }
 
-    /** Tests if a knight can move over 3 pawns in front
-     *          ...|x| |x|...
-     *          ...|P|P|P|...
-     *          ...| |N| |...
-     */
-    @Test
-    void isLegalMove_knightMovesOverOwnColourPieces1_True(){
-        Board board = new Board();
-        board.boardMap.clear();
+    @ParameterizedTest
+    @MethodSource("model.DataProvider#pieceProvider")
+    void isLegalMove_knightTakesDifferentColourPiece_True(BasePiece piece) {
+        BasePiece knight = new Knight(piece.colour.next());
+        board.boardMap.put(BE4, knight);
 
-        BasePiece blueKnight = new Knight(Colour.BLUE);
-        board.boardMap.put(BE2, blueKnight);
+        board.boardMap.put(BC3, piece);
 
-        BasePiece pawn1 = new Pawn(Colour.BLUE);
-        BasePiece pawn2 = new Pawn(Colour.BLUE);
-        BasePiece pawn3 = new Pawn(Colour.BLUE);
-
-        board.boardMap.put(BD3, pawn1);
-        board.boardMap.put(BE3, pawn2);
-        board.boardMap.put(BF3, pawn3);
-
-        assertTrue(blueKnight.isLegalMove(board, BE2, BD4));
-        assertTrue(blueKnight.isLegalMove(board, BE2, BF4));
+        assertTrue(knight.isLegalMove(board, BE4, BC3));
     }
 
 
-    /** Tests if a knight can move over 2 pawns in front
-     *          ...|x|P|x|...
-     *          ...| |P| |...
-     *          ...| |N| |...
-     */
-    @Test
-    void isLegalMove_knightMovesOverOwnColourPieces2_True(){
-        Board board = new Board();
-        board.boardMap.clear();
-
-        BasePiece blueKnight = new Knight(Colour.BLUE);
-        board.boardMap.put(BE2, blueKnight);
-
-        BasePiece pawn1 = new Pawn(Colour.BLUE);
-        BasePiece pawn2 = new Pawn(Colour.BLUE);
-
-        board.boardMap.put(BE3, pawn1);
-        board.boardMap.put(BE4, pawn2);
-
-        assertTrue(blueKnight.isLegalMove(board, BE2, BD4));
-        assertTrue(blueKnight.isLegalMove(board, BE2, BF4));
-    }
-
-    @Test
-    void getHighlightPolygons_validPolygons_presentInPolygonList1(){
+    @ParameterizedTest
+    @EnumSource(Colour.class)
+    void getHighlightPolygons_validPolygons_presentInPolygonList(Colour colour){
         Board board = new Board();
         board.boardMap.clear();
         Position startPosition = BE4;
 
-        BasePiece knight = new Knight(Colour.BLUE);
+        BasePiece knight = new Knight(colour);
         board.boardMap.put(startPosition, knight);
 
-        Set<Position> expectedKnightMoves = new HashSet<>(Arrays.asList(BG3, BF2, BD2, BC3, GF4, GE3, RB4, RC3, RF4, RE3));
-        Set<Position> actualKnightMoves = new HashSet<>(knight.getHighlightPolygons(board, startPosition));
-
-        assertEquals(expectedKnightMoves, actualKnightMoves);
-    }
-
-    @Test
-    void getHighlightPolygons_validPolygons_presentInPolygonList2(){
-        Board board = new Board();
-        board.boardMap.clear();
-        Position startPosition = BE4;
-
-        BasePiece pawn = new Pawn(Colour.BLUE);
-        board.boardMap.put(RB4, pawn);
-
-        BasePiece knight = new Knight(Colour.BLUE);
-        board.boardMap.put(startPosition, knight);
-
-        Set<Position> expectedKnightMoves = new HashSet<>(Arrays.asList(BG3, BF2, BD2, BC3, GF4, GE3, RC3, RF4, RE3));
-        Set<Position> actualKnightMoves = knight.getHighlightPolygons(board, startPosition);
-
-        assertEquals(expectedKnightMoves, actualKnightMoves);
-    }
-
-    @Test
-    void getHighlightPolygons_validPolygons_presentInPolygonList3(){
-        Board board = new Board();
-        board.boardMap.clear();
-        Position startPosition = BE4;
-
-        BasePiece pawn = new Pawn(Colour.RED);
-        board.boardMap.put(RB4, pawn);
-
-        BasePiece knight = new Knight(Colour.BLUE);
-        board.boardMap.put(startPosition, knight);
-
-        Set<Position> expectedKnightMoves = new HashSet<>(Arrays.asList(BG3, BF2, BD2, BC3, GF4, GE3, RB4, RC3, RF4, RE3));
+        Set<Position> expectedKnightMoves = ImmutableSet.of(BG3, BF2, BD2, BC3, GF4, GE3, RB4, RC3, RF4, RE3);
         Set<Position> actualKnightMoves = knight.getHighlightPolygons(board, startPosition);
 
         assertEquals(expectedKnightMoves, actualKnightMoves);
