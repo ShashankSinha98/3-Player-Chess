7638704a2ffa6e1a04f455df2b87bc76bfdd2da8
Stefan Schulze <stefan.schulze@siemens.com>
2024-01-25 21:55:41 +0100

    added parameterized tests in PawnTest.java

--- backend/src/test/java/model/PawnTest.java
+++ backend/src/test/java/model/PawnTest.java
@@ -1,11 +1,13 @@
 package model;
 
+import com.google.common.collect.ImmutableSet;
 import common.Colour;
 import common.Position;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.Set;
 
@@ -28,116 +30,91 @@ import static org.junit.jupiter.api.Assertions.*;
         assertNotEquals(0, pawn.directions.length);
     }
 
-    @Test
-     void isLegalMove_validMoves_True() {
-        Position[] startPositions = new Position[] {BH2, BE4};
-        Position[][] endPositions = new Position[][] {{BH3, BH4}, {RD4}};
+     @Test
+     void isLegalMove_pawnMoveForwardToEmptySquare_True() {
+         BasePiece pawn = new Pawn(Colour.BLUE);
+         board.boardMap.put(BE4, pawn);
 
-        for(int i=0; i<startPositions.length; i++) {
-            Board board = new Board();
-            Position start = startPositions[i];
-            BasePiece pawn = new Pawn(start.getColour());
-            board.boardMap.put(start, pawn);
-            Position[] ends = endPositions[i];
-            for(Position end: ends) {
-                assertTrue(pawn.isLegalMove(board, start, end));
-            }
-        }
-    }
+         assertTrue(pawn.isLegalMove(board, BE4, RD4));
+     }
 
-    @Test
-     void isLegalMove_invalidMoves_False() {
-        Position[] startPositions = new Position[] {BH2, BE4};
-        Position[][] endPositions = new Position[][] {{BG3, BH1}, {BE3}};
-
-        for(int i=0; i<startPositions.length; i++) {
-            Board board = new Board();
-            Position start = startPositions[i];
-            BasePiece pawn = new Pawn(start.getColour());
-            board.boardMap.put(start, pawn);
-            Position[] ends = endPositions[i];
-            for(Position end: ends) {
-                assertFalse(pawn.isLegalMove(board, start, end));
-            }
-        }
-    }
-
-    @Test
-     void isLegalMove_pawnAbsentFromStartPosition_False() {
-        BasePiece pawn = new Pawn(Colour.BLUE);
+    @ParameterizedTest
+    @EnumSource(Colour.class)
+     void isLegalMove_pawnAbsentFromStartPosition_False(Colour colour) {
+        BasePiece pawn = new Pawn(colour);
         assertFalse(pawn.isLegalMove(board, BE4, BD3));
     }
 
-    @Test
-     void isLegalMove_pawnMoveForwardToTakeOpponentPiece_False() {
+    @ParameterizedTest
+    @MethodSource("model.DataProvider#pieceProvider")
+     void isLegalMove_pawnMoveForwardToTakeOpponentPiece_False(BasePiece piece) {
         BasePiece pawn = new Pawn(Colour.BLUE);
-        board.boardMap.put(BE4, pawn);
+        Position startPosition = BE4;
+        Position endPosition = RD4;
 
-        BasePiece king = new King(Colour.RED);
-        board.boardMap.put(RD4, king);
+        board.boardMap.put(startPosition, pawn);
+
+        board.boardMap.put(endPosition, piece);
 
         assertFalse(pawn.isLegalMove(board, BE4, RD4));
     }
 
-    @Test
-     void isLegalMove_pawnMoveDiagonalToTakeOpponentPiece_True() {
+    @ParameterizedTest
+     @MethodSource("model.DataProvider#pieceProvider")
+     void isLegalMove_pawnMoveDiagonalToTakeOpponentPiece_True(BasePiece piece) {
+        if(piece.getColour()==Colour.BLUE) return;
+
         BasePiece pawn = new Pawn(Colour.BLUE);
-        board.boardMap.put(BE4, pawn);
 
-        BasePiece king = new King(Colour.RED);
-        board.boardMap.put(RC4, king);
+        Position startPosition = BE4;
+        Position endPosition = RC4;
 
-        assertTrue(pawn.isLegalMove(board, BE4, RC4));
+        board.boardMap.put(startPosition, pawn);
+
+        board.boardMap.put(endPosition, piece);
+
+        assertTrue(pawn.isLegalMove(board, startPosition, endPosition));
     }
 
-    @Test
-     void isLegalMove_pawnTakesItsColourPiece_False() {
-        BasePiece bluePawn = new Pawn(Colour.BLUE);
-        board.boardMap.put(BE4, bluePawn);
+     @ParameterizedTest
+     @MethodSource("model.DataProvider#pieceProvider")
+     void isLegalMove_pawnTakesItsColourPiece_False(BasePiece piece) {
+        if(piece.getColour() != Colour.BLUE) return;
 
-        BasePiece blueKing = new King(Colour.BLUE);
-        board.boardMap.put(RC4, blueKing);
+        BasePiece pawn = new Pawn(Colour.BLUE);
+        Position startPosition = BE4;
+        Position endPosition = RC4;
 
-        assertFalse(bluePawn.isLegalMove(board, BE4, RC4));
+        board.boardMap.put(startPosition, pawn);
+
+        board.boardMap.put(endPosition, piece);
+
+        assertFalse(pawn.isLegalMove(board, startPosition, endPosition));
     }
 
-    @Test
-     void getHighlightPolygons_validPolygons_presentInPolygonList() {
-        Position[] startPositions = new Position[] {BH2, BE4};
-        Position[][] endPositions = new Position[][] {{BH3, BH4}, {RD4}};
+     @Test
+     void getHighlightPolygons_pawnInitialPosition_presentInPolygonList() {
+         Position startPosition = BB2;
 
-        for(int i=0; i<startPositions.length; i++) {
-            Board board = new Board();
-            Position start = startPositions[i];
-            BasePiece pawn = new Pawn(start.getColour());
-            board.boardMap.put(start, pawn);
-            Position[] ends = endPositions[i];
+         BasePiece pawn = new Pawn(startPosition.getColour());
 
-            Set<Position> highlightedPolygons = pawn.getHighlightPolygons(board, start);
-            for(Position end: ends) {
-                assertTrue(highlightedPolygons.contains(end));
-            }
-        }
+         Set<Position> expectedPawnMoves = ImmutableSet.of(BB3, BB4);
+         Set<Position> actualPawnMoves = pawn.getHighlightPolygons(board, startPosition);
+
+        assertEquals(expectedPawnMoves, actualPawnMoves);
     }
 
-    @Test
-     void getHighlightPolygons_invalidPolygons_presentInPolygonList() {
-        Position[] startPositions = new Position[] {BH2, BE4};
-        Position[][] endPositions = new Position[][] {{BG3, BH1}, {BE3}};
+     @Test
+     void getHighlightPolygons_pawnAlreadyMoved_presentInPolygonList() {
+         Position startPosition = BE4;
 
-        for(int i=0; i<startPositions.length; i++) {
-            Board board = new Board();
-            Position start = startPositions[i];
-            BasePiece pawn = new Pawn(start.getColour());
-            board.boardMap.put(start, pawn);
-            Position[] ends = endPositions[i];
+         BasePiece pawn = new Pawn(startPosition.getColour());
 
-            Set<Position> highlightedPolygons = pawn.getHighlightPolygons(board, start);
-            for(Position end: ends) {
-                assertFalse(highlightedPolygons.contains(end));
-            }
-        }
-    }
+         Set<Position> expectedPawnMoves = ImmutableSet.of(RD4);
+         Set<Position> actualPawnMoves = pawn.getHighlightPolygons(board, startPosition);
+
+         assertEquals(expectedPawnMoves, actualPawnMoves);
+     }
 
     @ParameterizedTest
     @EnumSource(Colour.class)
