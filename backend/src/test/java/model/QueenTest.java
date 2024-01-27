467653c8b4ad803e324c541e3082a15ad7ac32be
Stefan Schulze <stefan.schulze@siemens.com>
2024-01-27 19:57:27 +0100

    added Javadoc to QueenTest.java

--- backend/src/test/java/model/QueenTest.java
+++ backend/src/test/java/model/QueenTest.java
@@ -15,23 +15,40 @@ import java.util.Set;
 import static common.Position.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * Test class for the {@link Queen} class.
+ * Contains various tests to verify the behavior of the Queen piece in the game.
+ */
  class QueenTest {
 
      private Board board;
      private Map<Position, BasePiece> boardMap;
 
+    /**
+     * Initializes a new Board instance before each test.
+     */
      @BeforeEach
      void initBeforeEachBoardTest() {
          board = new Board();
          boardMap = board.boardMap;
      }
 
+    /**
+     * Tests the setupDirections method,
+     * expecting the Queen movement directions to be non-empty.
+     */
     @Test
      void setupDirections_initPieceDirectionsIsEmpty_False() {
         BasePiece queen = new Queen(Colour.BLUE);
         assertNotEquals(0, queen.directions.length);
     }
 
+    /**
+     * Parameterized test for checking if the queen is present in its initial position,
+     * expecting true.
+     *
+     * @param position Initial position of the queen
+     */
      @ParameterizedTest
      @EnumSource(value = Position.class, names = {"BD1", "RD1", "GD1"})
      void check_queenPresentInInitialPosition_True(Position position) {
@@ -39,6 +56,11 @@ import static org.junit.jupiter.api.Assertions.*;
          assertInstanceOf(Queen.class, piece);
      }
 
+    /**
+     * Parameterized test for isLegalMove method when the queen moves to an empty square, expecting true.
+     *
+     * @param colour Colour of the queen
+     */
      @ParameterizedTest
      @EnumSource(Colour.class)
      void isLegalMove_queenMovesToEmptySquare_True(Colour colour) {
@@ -53,6 +75,13 @@ import static org.junit.jupiter.api.Assertions.*;
          assertTrue(actualQueenMoves.contains(GF3));
      }
 
+    /**
+     * Parameterized test for isLegalMove method
+     * when the queen takes a piece of its own colour,
+     * expecting false.
+     *
+     * @param piece Piece to be placed on the board
+     */
      @ParameterizedTest
      @MethodSource("model.DataProvider#pieceProvider")
      void isLegalMove_queenTakesItsColourPiece_False(BasePiece piece) {
@@ -67,6 +96,13 @@ import static org.junit.jupiter.api.Assertions.*;
          assertFalse(actualQueenMoves.contains(endPosition));
      }
 
+    /**
+     * Parameterized test for isLegalMove method
+     * when the queen takes a piece of a different colour,
+     * expecting true.
+     *
+     * @param piece Piece to be placed on the board
+     */
      @ParameterizedTest
      @MethodSource("model.DataProvider#pieceProvider")
      void isLegalMove_queenTakesDifferentColourPiece_True(BasePiece piece) {
@@ -81,6 +117,12 @@ import static org.junit.jupiter.api.Assertions.*;
          assertTrue(actualQueenMoves.contains(endPosition));
      }
 
+    /**
+     * Parameterized test for getHighlightPolygons method,
+     * expecting valid polygons to be present in the list.
+     *
+     * @param colour Colour of the queen
+     */
      @ParameterizedTest
      @EnumSource(Colour.class)
      void getHighlightPolygons_validPolygons_presentInPolygonList(Colour colour) {
@@ -98,6 +140,15 @@ import static org.junit.jupiter.api.Assertions.*;
          assertEquals(expectedQueenMoves, actualQueenMoves);
      }
 
+    /**
+     * Parameterized test for toString method,
+     * expecting correct string format for queen initialization.
+     * BQ: blue Queen
+     * GQ: green Queen
+     * RQ: red Queen
+     *
+     * @param colour Colour of the queen
+     */
     @ParameterizedTest
     @EnumSource(Colour.class)
     void toString_initQueenAllColours_correctStringFormat(Colour colour) {
