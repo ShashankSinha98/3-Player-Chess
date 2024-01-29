9cd9a688538f5bdbc63e4f4fad7cfcd1b1f5d630
Stefan Schulze <stefan.schulze@siemens.com>
2024-01-29 21:26:21 +0100

    added Javadoc to GameBuilder.java

--- backend/src/main/java/main/GameBuilder.java
+++ backend/src/main/java/main/GameBuilder.java
@@ -8,21 +8,40 @@ import utility.Settings;
 public class GameBuilder {
     private Settings settings = new Settings();
 
+    /**
+     * set the name of the blue player
+     * @param name the name of the blue player
+     * @return actual GameBuilder
+     */
     public GameBuilder setBluePlayerName(String name){
         settings.setBluePlayerName(name);
         return this;
     }
 
+    /**
+     * set the name of the green player
+     * @param name the name of the green player
+     * @return actual GameBuilder
+     */
     public GameBuilder setGreenPlayerName(String name){
         settings.setGreenPlayerName(name);
         return this;
     }
 
+    /**
+     * set the name of the red player
+     * @param name the name of the red player
+     * @return actual GameBuilder
+     */
     public GameBuilder setRedPlayerName(String name){
         settings.setRedPlayerName(name);
         return this;
     }
 
+    /**
+     * builds a new GameMain instance
+     * @return newly created GameMain instance
+     */
     public GameMain build() {
         GameMain game = new GameMain();
         game.setSettings(settings);
